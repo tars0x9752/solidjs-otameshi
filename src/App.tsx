09c236a2cfaa8e31de16450767850eedc4976822
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-06-23 22:29:45 +0900

    init2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import type { Component } from 'solid-js';
+import { Component, For } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
+  const list = [
+    {
+      id: 1,
+      text: 'asdf',
+    },
+    {
+      id: 2,
+      text: 'yoyoyo',
+    },
+    {
+      id: 3,
+      text: 'hogehoge'
+    }
+  ]
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div class={styles.app}>
+      <header>
+        <h1>Todo list</h1>
       </header>
+      <main>
+        <ul>
+          <For each={list}>
+            {(item) => {
+              return <li>{item.text}</li>
+            }}
+          </For>
+        </ul>
+      </main>
     </div>
   );
 };
