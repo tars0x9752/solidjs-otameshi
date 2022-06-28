1d38dd68c90dacf3c454d93ebfef974e599e5252
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-06-28 19:12:18 +0900

    add prett

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Component, For, Show, Switch, Match } from 'solid-js';
-import { createStore } from 'solid-js/store';
+import { Component, For, Show, Switch, Match } from 'solid-js'
+import { createStore } from 'solid-js/store'
 
-import styles from './App.module.css';
+import styles from './App.module.css'
 
 type TaskStatus = 'todo' | 'wip' | 'done'
 
 type Task = {
-  id: string;
-  taskText: string;
-  status: TaskStatus;
-  targetAt?: string;
-  doneAt?: string;
+  id: string
+  taskText: string
+  status: TaskStatus
+  targetAt?: string
+  doneAt?: string
 }
 
 const todoList: Task[] = [
@@ -30,7 +30,7 @@ const todoList: Task[] = [
     id: '3',
     status: 'todo',
     taskText: 'foo',
-  }
+  },
 ]
 
 const wipList: Task[] = [
@@ -38,7 +38,7 @@ const wipList: Task[] = [
     id: '4',
     status: 'wip',
     taskText: 'yoyo',
-  }
+  },
 ]
 
 const doneList: Task[] = [
@@ -53,64 +53,81 @@ const doneList: Task[] = [
     status: 'done',
     doneAt: '2022-06-02',
     taskText: 'zxzx',
-  }
+  },
 ]
 
-const TaskItem: Component<Task> = (props) => {
-  // const { status, taskText, targetAt, doneAt } = task;
-
-  return <div>
-    <p><b>task:</b> {props.taskText}
-      <Show when={props.targetAt}>
-        {
-          (_targetAt) => {
-            return <><br /><b>target:</b> {_targetAt}</>
-          }
-        }
-      </Show>
-      <Show when={props.doneAt}>
-        {
-          (_doneAt) => {
-            return <><br /><b>done:</b> {_doneAt}</>
-          }
-        }
-      </Show>
-      <Switch>
-        <Match when={props.status === "todo"}>
-          <br />
-          <button onClick={() => {
-            console.log('yo')
-          }}>wip</button>
-          <button onClick={() => { }}>done</button>
-        </Match>
-        <Match when={props.status === "wip"}>
-          <br />
-          <button>done</button>
-        </Match>
-      </Switch>
-    </p>
-  </div>
+const TaskItem: Component<Task> = props => {
+  return (
+    <div>
+      <p>
+        <b>task:</b> {props.taskText}
+        <Show when={props.targetAt}>
+          {_targetAt => {
+            return (
+              <>
+                <br />
+                <b>target:</b> {_targetAt}
+              </>
+            )
+          }}
+        </Show>
+        <Show when={props.doneAt}>
+          {_doneAt => {
+            return (
+              <>
+                <br />
+                <b>done:</b> {_doneAt}
+              </>
+            )
+          }}
+        </Show>
+        <Switch>
+          <Match when={props.status === 'todo'}>
+            <br />
+            <button
+              onClick={() => {
+                console.log('yo')
+              }}
+            >
+              wip
+            </button>
+            <button onClick={() => {}}>done</button>
+          </Match>
+          <Match when={props.status === 'wip'}>
+            <br />
+            <button>done</button>
+          </Match>
+        </Switch>
+      </p>
+    </div>
+  )
 }
 
-const TaskItemList: Component<{ list: Task[] }> = (props) => {
+const TaskItemList: Component<{ list: Task[] }> = props => {
   return (
     <For each={props.list}>
-      {
-        (item) => {
-          return <TaskItem {...item} />
-        }
-      }
+      {item => {
+        return <TaskItem {...item} />
+      }}
     </For>
   )
 }
 
 const AddTaskForm: Component<{ onClick: (id: string) => void }> = ({ onClick }) => {
-  return <><button onClick={() => {
-    onClick('6')
-  }}>add</button></>
+  return (
+    <>
+      <button
+        onClick={() => {
+          onClick('6')
+        }}
+      >
+        add
+      </button>
+    </>
+  )
 }
 
-const Pu: Component<{ fuga: number }> = (props) => {
+const Pu: Component<{ fuga: number }> = props => {
   return <p>{props.fuga}</p>
 }
 
@@ -120,19 +137,17 @@ const App: Component = () => {
     wipList,
     doneList,
     hoge: {
-      fuga: 1
-    }
+      fuga: 1,
+    },
   })
 
   const handle = () => {
-    setStore('hoge', 'fuga', (prev) => {
+    setStore('hoge', 'fuga', prev => {
       return prev + 1
-    }
-    )
+    })
     setStore('todoList', 0, 'taskText', 'updated')
     console.log('handle')
   }
-
 
   return (
     <div class={styles.app}>
@@ -159,7 +174,7 @@ const App: Component = () => {
         </div>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
